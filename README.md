272e77210a163a168d07314a88e81c520ef790e3
Udin <fghaza.pratama@gmail.com>
2022-08-16 12:37:06 +0700

    add webicon

--- README.md
+++ README.md
@@ -1,17 +1,22 @@
 # 🌐 Youtube Downloader Website
+
 A website to download a video from youtube, just paste and download
 
-# How to use 
+# How to use
+
 1. clone the project
 2. open the file in the terminal and run
+
 ```bash
 # install dependencies
 npm install
 
-# run 
+# run
 npm start
 ```
+
 3. open browser and go this link http://localhost:3000
 
 ### Note
+
 if you want to host it somewhere you need to change the url `line 40` in the `/pages/download.ejs`
--- app.js
+++ app.js
@@ -1,45 +1,47 @@
-const express = require('express');
-const cors = require('cors');
-const ytdl = require('ytdl-core');
-const { render } = require('ejs');
+const express = require("express");
+const cors = require("cors");
+const ytdl = require("ytdl-core");
+const { render } = require("ejs");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs');
-app.set('views', 'pages');
+app.set("view engine", "ejs");
+app.set("views", "pages");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.listen(port, () => {
-    console.log(`Listening http://localhost:${port}`);
+  console.log(`Listening http://localhost:${port}`);
 });
 
-app.get('/', (req, res) => {
-    res.render('download');
+app.get("/", (req, res) => {
+  res.render("download");
 });
 
-app.get('/download', async (req, res) => {
-    try {
-        const URL = req.query.URL;
-        
-        const video_info = await ytdl.getInfo(URL);
-        
-        const video_title = video_info.videoDetails.title;
-         
-        res.header('Content-Disposition', 'attachment; filename=' + video_title + '.mp4');
+app.get("/download", async (req, res) => {
+  try {
+    const URL = req.query.URL;
 
-        // res.header('Content-Length', 'attachment; filesize=' + video_size + 'MB');
+    const video_info = await ytdl.getInfo(URL);
 
-        ytdl(URL, {
-            filter: format => format.container === 'mp4',
-            filter: 'audioandvideo',
-            quality: 'highestvideo',
-        }).pipe(res);
-    } catch (error) {
-        console.log(`===> oops there's an error apperently! <====`);
-        console.log(error);
-    };
+    const video_title = video_info.videoDetails.title;
+
+    // const video_size = video_info.formats.filter((item) => item.container === "mp4");
+    // console.log(video_size);
+
+    res.header("Content-Disposition", "attachment; filename=" + video_title + ".mp4");
+
+    // res.header("Content-Length", "attachment; filesize=" + video_size + " MB");
+
+    ytdl(URL, {
+      filter: (format) => format.container === "mp4",
+      filter: "audioandvideo",
+      quality: "highestvideo",
+    }).pipe(res);
+  } catch (error) {
+    console.error(error);
+  }
 });
